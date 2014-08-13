b2cd9c6df0abc9db26acc15b765abded2528a023
Regynald Ventura <regynaldventura@gmail.com>
2014-08-14 01:14:56 +0800

    major revision.. trimmed most of the code

--- start-socket-server.js
+++ start-socket-server.js
@@ -1,24 +1,19 @@
-var app = require( "express" )( );
-var server = require( "http" ).Server( app );
-var io = require( "socket.io" )( server );
-
 var startSocketServer = function startSocketServer( host, port, channelSet, boundProxyServerURL ){
 
-	server.listen( port, host );
-
-	server.on ( "listening", function onListen( ){
-		console.log ("Server Online: listening on " + host + ":" + port );
-	} );
+	server.listen(port,host);
 
 	io.on( "connection", function onConnection( socket ){
-		console.log( socket.id + ":connected" );
-
-		socket.on( "disconnect", function onDisconnect( ){
-			console.log( socket.id  + " disconnected." );
+		socket.on( "command", function onCommand( command ){
+			console.log( "@cli:"+command );
 		} );
+	} );
 
-	} );	
 };
+
+var app = require( "express" )( );
+var server = require( "http" ).Server( app) ;
+var io = require( "socket.io" )( server );
+
 exports.startSocketServer = startSocketServer;
 
 startSocketServer("127.0.0.1","3000","","");
