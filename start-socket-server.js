e8fadb39af8f0fba94ff62800c361f6bb2bd920b
Regynald Ventura <regynaldventura@gmail.com>
2014-08-14 18:10:06 +0800

    added a simple proxy redirect

--- start-socket-server.js
+++ start-socket-server.js
@@ -1,29 +1,37 @@
 var startSocketServer = function startSocketServer( host, port, channelSet, boundProxyServerURL ){
 
-	server.listen( port, host );
-
-	app.use( function onUse( request, response, next ){
-		next("ok");
-	} );
-	
-	var command = io
-	.of( "/command" )
-	.on( "connection",
-		function onConnect( socket ){
-			socket.on( "cli", function onCommand( command ){
-				console.log("@cli:"+command);
-			} );
-
-			command.emit( "cli", "command" );
+				
+		app.use( function onUse( request, response, next ){
+			if (!( /command/.test( request.url ) ) ){
+				next ( proxy.web( request, response, { target: "http://127.0.0.1:8000/command" } ) );
+			}else{				
+				//for testing
+				response.writeHead(200, { "Content-Type": "text/plain" } );
+				response.end( "request to server successful" + "\n" + JSON.stringify( request.headers, true, 2) );
+			}
 		} );
 
+		var command = io
+		.of( "/command" )
+		.on( "connection",
+			function onConnect( socket ){
+				socket.on( "cli", function onCommand( command ){
+					console.log("@cli:"+command);
+				} );
 
+				command.emit( "cli", "command" );
+			} );
+
+		server.listen( port, host );
+		var proxy = proxyServer( "127.0.0.1","9000","" );
 };
 
 var app = require( "express" )( );
 var server = require( "http" ).Server( app );
 var io = require( "socket.io" )( server );
 
-exports.startSocketServer = startSocketServer;
+var proxyServer = require( "../start-proxy-server/start-proxy-server.js" ).startProxyServer;
 
-startSocketServer("127.0.0.1","3000","","");
+exports.startSocketServer = startSocketServer;
+	
+startSocketServer("127.0.0.1","9000","","");
