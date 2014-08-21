189d14e360c311c6e70a12434f9ced221a5deff5
Richeve S. Bebedor <volkovasystems@gmail.com>
2014-08-21 22:51:48 +0800

    Updated with license shit. Updated with still on plan changes.

--- start-socket-server.js
+++ start-socket-server.js
@@ -1,37 +1,84 @@
+/*:
+	@module-license:
+		The MIT License (MIT)
+
+		Copyright (c) 2014 Regynald Reiner Ventura
+		Copyright (c) 2014 Richeve Siodina Bebedor
+
+		Permission is hereby granted, free of charge, to any person obtaining a copy
+		of this software and associated documentation files (the "Software"), to deal
+		in the Software without restriction, including without limitation the rights
+		to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+		copies of the Software, and to permit persons to whom the Software is
+		furnished to do so, subject to the following conditions:
+
+		The above copyright notice and this permission notice shall be included in all
+		copies or substantial portions of the Software.
+
+		THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+		IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+		FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+		AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+		LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+		OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+		SOFTWARE.
+	@end-module-license
+
+	@module-configuration:
+		{
+			"packageName": "start-socket-server",
+			"fileName": "start-socket-server.js",
+			"moduleName": "startSocketServer",
+			"authorName": "Regynald Reiner Ventura",
+			"authorEMail": "regynaldventura@gmail.com",
+			"contributorList": [
+				{
+					"contributorName": "Richeve Siodina Bebedor",
+					"contributorEMail": "richeve.bebedor@gmail.com"
+				}
+			],
+			"repository": "git@github.com:volkovasystems/start-socket-server.git",
+			"testCase": "start-socket-server-test.js",
+			"isGlobal": true
+		}
+	@end-module-configuration
+
+	@module-documentation:
+		
+	@end-module-documentation
+
+	@include:
+		{			
+			
+		}
+	@end-include
+*/
 var startSocketServer = function startSocketServer( host, port, channelSet, boundProxyServerURL ){
 
-				
-		app.use( function onUse( request, response, next ){
-			if (!( /command/.test( request.url ) ) ){
-				next ( proxy.web( request, response, { target: "http://127.0.0.1:8000/command" } ) );
-			}else{				
-				//for testing
-				response.writeHead(200, { "Content-Type": "text/plain" } );
-				response.end( "request to server successful" + "\n" + JSON.stringify( request.headers, true, 2) );
-			}
-		} );
-
-		var command = io
+	var server = http.createServer( );
+	var socket = socketio( server );
+			
+	socket
 		.of( "/command" )
 		.on( "connection",
-			function onConnect( socket ){
-				socket.on( "cli", function onCommand( command ){
-					console.log("@cli:"+command);
-				} );
+			function onConnect( liveSocket ){
 
-				command.emit( "cli", "command" );
+				liveSocket.on( "cli", 
+					function onCommand( command ){
+						console.log( "@cli:" + command );
+					} );
+
+				liveSocket.on( "socket",
+					function onSocket( ){
+
+					} );
 			} );
 
-		server.listen( port, host );
-		var proxy = proxyServer( "127.0.0.1","9000","" );
+	server.listen( port, host );
 };
 
-var app = require( "express" )( );
-var server = require( "http" ).Server( app );
-var io = require( "socket.io" )( server );
 
-var proxyServer = require( "../start-proxy-server/start-proxy-server.js" ).startProxyServer;
+var http = require( "http" );
+var socketio = require( "socket.io" );
 
-exports.startSocketServer = startSocketServer;
-	
-startSocketServer("127.0.0.1","9000","","");
+module.exports = startSocketServer;
